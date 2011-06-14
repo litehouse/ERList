b35d0b70a27da45193c7c4a44bf607b3795cdbcc
litehouse <litehouse.dev@gmail.com>
2011-06-14 13:49:08 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ERList::Application.config.secret_token = '107fc100fa180b037e0ece71d2d60def28ce4165554e3128701ca40327ced232aaf5adadd0f229d776db2c043f771815b402a034c839115a3331e8669984f6c9'
