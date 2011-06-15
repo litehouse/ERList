d84388625f1caf8062ad4085eaa99b77d935d974
litehouse <litehouse.dev@gmail.com>
2011-06-15 15:04:04 +0200

    amazon en fb links added

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,8 @@
 class VotesController < ApplicationController
+    
+  before_filter :authenticate, :except => [:destroy, :create]
   
-  def create
+    def create
       @book = Book.find(params[:vote][:voted_id])
       current_user.vote!(@book)
       redirect_to @book
